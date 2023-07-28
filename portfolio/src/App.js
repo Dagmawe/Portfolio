62c833021e09e01b88b6e05f8b30a2785563a734
Dagmawe <legesse.dagmawe@gmail.com>
2023-07-28 15:00:24 -0400

    portfolio

---
+++ portfolio/src/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { BrowserRouter as Router } from "react-router-dom";
+import Header from "./components/Header";
+import AnimRoutes from "./components/AnimRoutes";
+import vid from "./img/header/vid-1.mp4";
+import vid2 from "./img/header/video-2.mp4";
+const App = () => {
+  return (
+    <>
+      <video src={vid} loop autoPlay muted id="bg_vid" />
+      <Router>
+        <Header />
+        <AnimRoutes />
+      </Router>
+    </>
+  );
+};
+
+export default App;
---
+++ portfolio/src/pages/Home.js
@@ -0,0 +1,38 @@
+import React from "react";
+import { motion } from "framer-motion";
+import { transition1 } from "../transitions";
+
+const Home = () => {
+  return (
+    <>
+      <motion.section
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        exit={{ opacity: 0 }}
+        transition={transition1}
+        className="section"
+      >
+        <div className="container flex-col mx-auto h-max relative">
+          <div className="h-screen">
+            <motion.div
+              initial={{ opacity: 0, y: "-50%" }}
+              animate={{ opacity: 1, y: 0 }}
+              exit={{ opacity: 0, y: "-50%" }}
+              transition={transition1}
+              className="w-full pt-36 lg:pt-50 pb-5 lg:pb-0 lg:w-auto z-10 lg:absolute flex flex-col justify-center items-center lg:items-start "
+            >
+              <h1 className="h1 mb-40 mt-5 font-extrabold shadow-md ">
+                Dagmawe Legesse
+              </h1>
+              <p className="text-[26px] lg:text-[36px] font-primary mb-5 shadow-md">
+                Software Engineer
+              </p>
+            </motion.div>
+          </div>
+        </div>
+      </motion.section>
+    </>
+  );
+};
+
+export default Home;
